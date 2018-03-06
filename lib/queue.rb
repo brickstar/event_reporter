4fce54046e790b1373bc522d50f8be21ac129b0a
brickstar <brickstar@gmail.com>
2018-03-06 12:26:08 -0700

    working on q class methods

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,6 +1,23 @@
 class Q
-  attr_reader :queue
+  attr_reader :queue,
+              :counter
   def initialize
     @queue = []
+  end
+
+  def clear
+    @queue.clear
+  end
+
+  def print
+    
+  end
+
+  def print_by(attribute)
+
+  end
+
+  def count
+    @queue.length
   end
 end
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 require 'pry'
 
 
-class QueueTest < Minitest::Test
+class QTest < Minitest::Test
 
   def test_it_exists
     q = Q.new
@@ -18,5 +18,20 @@ class QueueTest < Minitest::Test
     assert_equal [], q.queue
   end
 
-  
+  def test_count_starts_at_0
+    q = Q.new
+
+    assert_equal 0, q.queue.length
+  end
+
+  def test_it_can_clear_queue
+    q = Q.new
+    q.queue.push [55]
+
+    assert_equal 1, q.queue.length
+
+    q.clear
+
+    assert_equal 0, q.queue.length
+  end
 end
