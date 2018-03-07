a61639e385fba0ff01e6d00ac4a9d2638bc611be
brickstar <brickstar@gmail.com>
2018-03-06 21:24:46 -0700

    print working, not formatted correctly, needs test

--- lib/queue.rb
+++ lib/queue.rb
@@ -9,13 +9,25 @@ class Q
     @queue.clear
   end
 
-  def add(attendee)
-    @queue << attendee
+  def add(data)
+    @queue << data
   end
 
-  def print
+  def find(attribute, criteria)
 
   end
+
+  def print_headers
+    "LAST NAME\tFIRST NAME\tEMAIL\tZIPCODE\tCITY\tSTATE\tADDRESS\tPHONE\tDISTRICT"
+  end
+
+  def print_stuff
+    puts print_headers
+    queue.find_all do |attendee|
+      puts "#{attendee.last_name}\t#{attendee.first_name}\t#{attendee.email_address}\t#{attendee.city}\t#{attendee.state}\t#{attendee.street}\t#{attendee.homephone}\tDISTRICT"
+    end
+  end
+
 
   def print_by(attribute)
 
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -27,6 +27,7 @@ class QTest < Minitest::Test
 
   def test_it_can_add_to_the_queue
     q = Q.new
+
     amanda = Attendee.new({ id: '19',
                             regdate: '11/23/08 20:44',
                             first_name: 'Amanda',
@@ -41,6 +42,24 @@ class QTest < Minitest::Test
 
     assert_equal 1, q.queue.length
   end
+
+  def test_it_can_count
+    q = Q.new
+    amanda = Attendee.new({ id: '19',
+                            regdate: '11/23/08 20:44',
+                            first_name: 'Amanda',
+                            last_name: 'Hartzell',
+                            email_address: 'nqm17@jumpstartlab.com',
+                            homephone: '607-280-2000',
+                            street: '3515 School St',
+                            city: 'Valois',
+                            state: 'NY',
+                            zipcode: '14841' })
+
+      q.add(amanda)
+
+      assert_equal 1, q.queue.length
+    end
 
   def test_it_can_clear_queue
     q = Q.new
@@ -61,5 +80,36 @@ class QTest < Minitest::Test
     q.clear
 
     assert_equal 0, q.queue.length
+  end
+
+  def test_print_headers
+
+  end
+  def test_print_stuff
+    q = Q.new
+    amanda = Attendee.new({ id: '19',
+                            regdate: '11/23/08 20:44',
+                            first_name: 'Amanda',
+                            last_name: 'Hartzell',
+                            email_address: 'nqm17@jumpstartlab.com',
+                            homephone: '607-280-2000',
+                            street: '3515 School St',
+                            city: 'Valois',
+                            state: 'NY',
+                            zipcode: '14841' })
+
+    dingleberry = Attendee.new({ id: '20',
+                             regdate: '12/24/09 21:44',
+                             first_name: 'Dingle',
+                             last_name: 'Berry',
+                             email_address: 'dingle@dingle.com',
+                             homephone: '555-867-5309',
+                             street: '321 Bebop St',
+                             city: 'Denver',
+                             state: 'CO',
+                             zipcode: '80218' })
+    q.add(amanda)
+    q.add(dingleberry)
+binding.pry
   end
 end
