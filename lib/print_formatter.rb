edc2d12da538b1f97ced0133ff684e7cc34edf33
brickstar <brickstar@gmail.com>
2018-03-07 23:20:02 -0700

    printformatter module testing

---
+++ lib/print_formatter.rb
@@ -0,0 +1,30 @@
+
+
+module PrintFormatter
+
+  def headers
+    ["LAST NAME", "FIRST NAME", "EMAIL", "ZIPCODE", "CITY", "STATE", "ADDRESS", "PHONE", "DISTRICT"]
+  end
+
+  def create_attendee_array
+    queue.map do |attendee|
+      [attendee.last_name, attendee.first_name, attendee.email_address, attendee.zipcode, attendee.city, attendee.state, attendee.street, attendee.homephone, "DISTRICT"]
+    end
+  end
+
+  def create_line(words, length)
+    words.map do |word| word.ljust(length)
+    end.join(" ")
+  end
+
+  def find_max_characters
+    create_attendee_array.flatten.reduce(0) do |length, string|
+      if string.length > length
+        string.length
+      else
+        length
+      end
+    end
+  end
+
+end
